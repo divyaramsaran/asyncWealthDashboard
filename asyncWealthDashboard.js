4f615e57e3049b165e5bc5e7c92e834b08820a99
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-07-08 15:18:39 +0000

    adds users

---
+++ asyncWealthDashboard.js
@@ -0,0 +1,17 @@
+const users = {
+  0: {
+    name: "John Doe",
+    age: 30,
+    email: "john.doe@example.com",
+  },
+  1: {
+    name: "Jane Smith",
+    age: 25,
+    email: "jane.smith@example.com",
+  },
+  2: {
+    name: "Alice Johnson",
+    age: 28,
+    email: "alice.johnson@example.com",
+  },
+};
