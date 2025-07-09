289a13ba6e92d2e52078b2428e126d1f573681b4
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-07-09 15:18:36 +0000

    adds validation of userId with hardCoded values and moves uses to function

--- asyncWealthDashboard.js
+++ asyncWealthDashboard.js
@@ -1,17 +1,28 @@
-const users = {
-  0: {
-    name: "John Doe",
-    age: 30,
-    email: "john.doe@example.com",
-  },
-  1: {
-    name: "Jane Smith",
-    age: 25,
-    email: "jane.smith@example.com",
-  },
-  2: {
-    name: "Alice Johnson",
-    age: 28,
-    email: "alice.johnson@example.com",
-  },
+const validateAndGetUserId = () => {
+  const userId = Number(prompt("Enter User Id"));
+
+  return userId >= 0 && userId <= 3 ? userId : validateAndGetUserId();
 };
+
+const getUserProfile = () => {
+  const users = {
+    0: {
+      name: "John Doe",
+      age: 30,
+      email: "john.doe@example.com",
+    },
+    1: {
+      name: "Jane Smith",
+      age: 25,
+      email: "jane.smith@example.com",
+    },
+    2: {
+      name: "Alice Johnson",
+      age: 28,
+      email: "alice.johnson@example.com",
+    },
+  };
+  return users[validateAndGetUserId()];
+};
+
+console.log(getUserProfile());
